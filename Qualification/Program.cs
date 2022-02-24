35ce185461f8cfaef369c5f893d7e873253b1b31
NikosGour <63046380+NikosGour@users.noreply.github.com>
2022-02-24 22:54:46 +0200

    optimize

--- Qualification/Program.cs
+++ Qualification/Program.cs
@@ -14,26 +14,20 @@ internal static class Files
 
 internal static class Program
 {
-    static readonly string ASSEMBLY_PATH    = AppDomain.CurrentDomain.BaseDirectory;
+    static readonly string ASSEMBLY_PATH = AppDomain.CurrentDomain.BaseDirectory;
     static readonly string PROJECT_DIR_PATH = ASSEMBLY_PATH.Substring(0, ASSEMBLY_PATH.IndexOf("\\bin"));
-    static readonly string INPUTS_DIR_PATH  = Path.Combine(PROJECT_DIR_PATH, "inputs");
+    static readonly string INPUTS_DIR_PATH = Path.Combine(PROJECT_DIR_PATH, "inputs");
 
 
     public static void Main()
     {
-        Main2();
-        Console.ReadLine();
-    }
-
-    public static void Main2()
-    {
         Directory.SetCurrentDirectory(INPUTS_DIR_PATH);
 
-        const string FILE = Files.B;
+        const string FILE = Files.C;
 
-        int           num_of_contributors, num_of_projects;
+        int num_of_contributors, num_of_projects;
         Contributor[] contributors;
-        Project[]     projects;
+        Project[] projects;
 
         Stopwatch sw = Stopwatch.StartNew();
 
@@ -44,17 +38,17 @@ internal static class Program
             var first_line = file.ReadLine()!.Split(' ');
 
             num_of_contributors = int.Parse(first_line[0]);
-            num_of_projects     = int.Parse(first_line[1]);
+            num_of_projects = int.Parse(first_line[1]);
 
 
             contributors = new Contributor[num_of_contributors];
-            projects     = new Project[num_of_projects];
+            projects = new Project[num_of_projects];
 
             for (int i = 0; i < num_of_contributors; i++)
             {
                 var name_and_skills_s = file.ReadLine()!.Split(' ');
 
-                var name          = name_and_skills_s[0];
+                var name = name_and_skills_s[0];
                 var num_of_skills = int.Parse(name_and_skills_s[1]);
 
                 Contributor contributor = new(name);
@@ -72,11 +66,11 @@ internal static class Program
             {
                 var first_line_s = file.ReadLine()!.Split(' ');
 
-                var name                       = first_line_s[0];
+                var name = first_line_s[0];
                 var number_of_days_to_complete = int.Parse(first_line_s[1]);
-                var score                      = int.Parse(first_line_s[2]);
-                var best_before                = int.Parse(first_line_s[3]);
-                var num_of_skills              = int.Parse(first_line_s[4]);
+                var score = int.Parse(first_line_s[2]);
+                var best_before = int.Parse(first_line_s[3]);
+                var num_of_skills = int.Parse(first_line_s[4]);
 
                 Project project = new(name, number_of_days_to_complete, score, best_before);
 
@@ -87,7 +81,7 @@ internal static class Program
                 }
 
                 project.contributors = new Contributor[project.skills.Count];
-                projects[i]          = project;
+                projects[i] = project;
             }
         }
 
@@ -100,7 +94,7 @@ internal static class Program
                     orderby project.days_to_complete
                     select project).ToArray();
         int completed_projects = 0;
-        int days               = 0;
+        int days = 0;
 
 
         string output = "";
@@ -115,8 +109,56 @@ internal static class Program
                     continue;
                 }
 
+                if (project.running)
+                {
+                    project.days_to_complete--;
+                    if (project.days_to_complete == 0)
+                    {
+                        output += project.name + "\n";
+                        for (var j = 0; j < project.contributors.Length; j++)
+                        {
+                            var contributor = project.contributors[j];
+                            output += $"{contributor.name} ";
+
+
+                            Skill project_skill = project.skills[j];
+                            if (contributor.skills.Contains(project_skill))
+                            {
+                                if (project_skill.level
+                                 >= contributor.skills.Find(x => x.name == project_skill.name)!.level)
+                                {
+                                    contributor.skills.Find(x => x.name == project_skill.name)!.level++;
+                                }
+                            }
+                            else
+                            {
+                                contributor.skills.Add(new Skill(project_skill.name, 1));
+                            }
+
+
+                            contributor.is_occupied = false;
+                            project.contributors[j] = null;
+                        }
+
+                        output += "\n";
+
+
+                        completed_projects++;
+                        projects[i] = null;
+                        days++;
+                        continue;
+                    }
+                }
+
+
+
                 for (int j = 0; j < project.skills.Count; j++)
                 {
+                    if (project.contributors[j] is not null)
+                    {
+                        continue;
+                    }
+
                     Skill skill = project.skills[j];
                     for (int k = 0; k < num_of_contributors; k++)
                     {
@@ -176,7 +218,7 @@ internal static class Program
                                                 == contributors.ToList()
                                                                .Min(x => x.skills.Count))!;
 
-                                    project.contributors[j]        = less_capable_contr;
+                                    project.contributors[j] = less_capable_contr;
                                     less_capable_contr.is_occupied = true;
                                     break;
                                 }
@@ -188,7 +230,7 @@ internal static class Program
                                     {
                                         if (!potential_contributor.is_occupied)
                                         {
-                                            project.contributors[j]           = potential_contributor;
+                                            project.contributors[j] = potential_contributor;
                                             potential_contributor.is_occupied = true;
                                             break;
                                         }
@@ -218,49 +260,12 @@ internal static class Program
                 }
 
 
-                if (project.running)
-                {
-                    project.days_to_complete--;
-                    if (project.days_to_complete == 0)
-                    {
-                        output += project.name + "\n";
-                        for (var j = 0; j < project.contributors.Length; j++)
-                        {
-                            var contributor = project.contributors[j];
-                            output += $"{contributor.name} ";
 
-
-                            Skill project_skill = project.skills[j];
-                            if (contributor.skills.Contains(project_skill))
-                            {
-                                if (project_skill.level
-                                 >= contributor.skills.Find(x => x.name == project_skill.name)!.level)
-                                {
-                                    contributor.skills.Find(x => x.name == project_skill.name)!.level++;
-                                }
-                            }
-                            else
-                            {
-                                contributor.skills.Add(new Skill(project_skill.name, 1));
-                            }
-
-
-                            contributor.is_occupied = false;
-                            project.contributors[j] = null;
-                        }
-
-                        output += "\n";
-
-
-                        completed_projects++;
-                        projects[i] = null;
-                    }
-                }
             }
 
             Console.WriteLine(days);
             days++;
-        } while (days < 5000); //while (completed_projects < num_of_projects);
+        } while (days < 10); //while (completed_projects < num_of_projects);
 
         Console.WriteLine("Finished\n~~~~~~~~~~~~");
 
@@ -273,9 +278,9 @@ internal static class Program
 
 internal class Contributor
 {
-    public string      name        { get; set; }
-    public bool        is_occupied { get; set; } = false;
-    public List<Skill> skills      { get; set; } = new();
+    public string name { get; set; }
+    public bool is_occupied { get; set; } = false;
+    public List<Skill> skills { get; set; } = new();
 
     public Contributor(string name)
     {
@@ -285,33 +290,33 @@ internal class Contributor
 
 internal class Project
 {
-    public string name             { get; set; }
-    public int    days_to_complete { get; set; }
-    public int    score            { get; set; }
-    public int    best_before      { get; set; }
+    public string name { get; set; }
+    public int days_to_complete { get; set; }
+    public int score { get; set; }
+    public int best_before { get; set; }
 
     public bool running { get; set; } = false;
 
     public Contributor[] contributors { get; set; }
-    public List<Skill>   skills       { get; set; } = new();
+    public List<Skill> skills { get; set; } = new();
 
     public Project(string name, int daysToComplete, int score, int bestBefore)
     {
-        this.name        = name;
+        this.name = name;
         days_to_complete = daysToComplete;
-        this.score       = score;
-        best_before      = bestBefore;
+        this.score = score;
+        best_before = bestBefore;
     }
 }
 
 record Skill()
 {
-    public string name  { get; set; }
-    public int    level { get; set; }
+    public string name { get; set; }
+    public int level { get; set; }
 
     public Skill(string name, int level) : this()
     {
-        this.name  = name;
+        this.name = name;
         this.level = level;
     }
 
